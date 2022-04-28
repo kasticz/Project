b0347c8621e1a170a15094d1253ec2b7ec08114f
kasticz <den-sendmen@yandex.ru>
2022-04-28 13:27:36 +0300

    final?

--- src/JS/script.js
+++ src/JS/script.js
@@ -157,7 +157,21 @@ $(document).ready(function(){
       //     $(`form`).trigger(`reset`);
       //   })
       //   return false;
-      // })      
+      // })    
+      
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 1600){
+          $(`.pageup`).fadeIn()
+        }else{
+          $(`.pageup`).fadeOut()
+        }
+      })
+      $("a[href='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      })
+      new WOW().init()
   });
 
 
--- zxc.js
+++ zxc.js
@@ -1,44 +1,10 @@
+for(var i = 0; i < 10; i++){
+    function x(i){
+        return () =>{
+            console.log(i)
+        } 
 
-function dec2FactString(nb) {
-    let alph = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
-    let num = nb;
-    let res = `0`;
-    let del = 2;
-    while(num > 0){  
-        if(num % del > 9){
-            res += alph.indexOf(num % del - 10)
-        } else{
-            res += num % del
-        }
-        num = Math.floor(num / del);
-        del++;
-        if(del > 20){
-            break;
-        }
     }
-    return res.split(``).reverse().join(``);
+    setTimeout(x(i), 1000);
+
 }
-function factString2Dec(str) {
-    let alph = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
-    let res;
-    if(+str[0] == str[0]){
-     res = +str[0];
-    }else{
-        res = alph.indexOf(str[0]) + 10
-    }
-    for(let i = 1; i < str.length - 1;i++){
-        res = (res * (str.length - i))
-        if(str[i] == +str[i]){
-            res += +str[i]  
-        }else{
-            res += alph.indexOf(str[i]) + 10;
-        }      
-    }
-    return res;
-}
-
-
-
-console.log(factString2Dec(`341010`))
-console.log(dec2FactString(463))
-
